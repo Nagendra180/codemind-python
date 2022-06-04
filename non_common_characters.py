6332d35e48f2cbbb722a59bbbafc9deb1c118f6b
Nagendra180 <106660151+Nagendra180@users.noreply.github.com>
2022-06-04 11:18:54 +0530

    n=input().lower() m=input().lower() a=[i for i in n if i not in m and i!=' ' and i.isalpha()] b=[i for i in m if i not in n and i!=' ' and i.isalpha()] a.extend(b) c=set(a) print(''.join(sorted(c)))

---
+++ non_common_characters.py
@@ -0,0 +1,7 @@
+n=input().lower()
+m=input().lower()
+a=[i for i in n if i not in m and i!=' ' and i.isalpha()]
+b=[i for i in m if i not in n and i!=' ' and i.isalpha()]
+a.extend(b)
+c=set(a)
+print(''.join(sorted(c)))
